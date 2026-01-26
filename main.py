51ab8eb7402666754ccc8234f66ae0d0f5d87b21
Bryce Hart <bryce.hart23@gmail.com>
2026-01-25 23:16:01 -0500

    Add random word and number generation functions
    Implement functions to generate random words and numbers, and create random lines for file writing.

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
+import random
+#maybe use wonderwords
 
+
+def getRandomWord():
+    return
+
+
+
+def getRandom():
+    ran = random.randint(0,3)
+    if ran > 2:
+        return random.randint(-9,999,999, 999,999,999) #for now between this range
+    else:
+        return getRandomWord
+
+def createRandomLine():
+    randomStr = ""
+    for i in range(10):
+        randomStr += getRandom
+        return getRandom
+
+
+
+
+def main():
+    RandomFileName = "RandomFile"
+
+    for i in range(5):
+        fileNumber = random.randint(0, 9)
+        RandomFileName + fileNumber
+
+    fileLength = random.randomint(1, 1000)
+    try:
+        with open(RandomFileName, "w") as file:
+            for i in range(fileLength):
+                file.writelines(createRandomLine)
+    except FileExistsError:
+        print("FileExistsError")
